54aa399e48946b20ef515c0865a6c23e44fd90a5
Nicolas Espel <nicolasespelcoding@gmail.com>
2023-03-09 16:21:14 -0500

    Adding existing proect files into a git repository

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("btn").addEventListener("click" , () => {
+    document.getElementById("result").innerHTML = "hello my name is nicolasfjdoisajf osdijafodisja jifdoas jdifo djsoajf iojsd oafijsd ajifoid jsafijdosa fidjsa ofjsodifj osdij"
+})
